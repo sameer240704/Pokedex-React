554f6926bae515be6a06be38641d86bdf7956029
sameer240704 <sameergupta79711@gmail.com>
2024-02-10 10:05:51 +0530

    Designed the front side when not open, remaining to add the buttons and lights

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="h-lvh w-screen bg-[url('/assets/background-image.jpg')] bg-center bg-cover bg-no-repeat">
-      {isOpen ? (
-        <div className="h-[500px] w-[500px] bg-red-700">
-          <div>Hello World</div>
+    <div className="h-lvh w-screen bg-[url('/assets/background-image.jpg')] bg-center bg-cover bg-no-repeat flex items-center justify-center">
+      {!isOpen ? (
+        <div className="h-[600px] w-[500px] bg-[#DC0A2D] border-4 border-[#99061F] rounded-3xl relative flex">
+          <div className="h-[446px] w-[300px] bg-transparent border-4 border-[#99061F] absolute top-[120px] border-x-0 border-b-0">
+          </div>
+          <div className="h-[480px] w-[100px] bg-transparent border-4 border-[#99061F] absolute top-[63.5px] left-[411.5px] border-x-0 border-b-0 rotate-[-30deg]"></div>
+          <div className="h-[480px] w-[110px] bg-transparent border-4 border-[#99061F] absolute top-[70.5px] left-[384px] border-x-0 border-b-0"></div>
         </div>
       ) : null}
     </div>
--- pokedex/tailwind.config.js
+++ pokedex/tailwind.config.js
@@ -7,14 +7,31 @@ module.exports = {
         "background-image": "url('/assets/background-image.jpg')",
       },
       colors: {
-        "regal-blue": "#243c5a",
+        "regal-blue": "#243C5A",
+        "pokemon-red": "#DC0A2D",
+        "pokemon-border-red": "#99061F",
       },
       height: {
         "156": "600px",
+        "130": "446px",
+        "140": "480px"
       },
       width: {
         "128": "500px",
+        "100": "411.5px",
+        "40": "40px",
+        "80": "150px"
       },
+      spacing: {
+        "150px": "150px",
+        "70.5px": "70.5px"
+      },
+      margin: {
+        
+      },
+      rotate: {
+        "21.4": "21.4deg"
+      }
     },
   },
   plugins: [],
